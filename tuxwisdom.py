388f794fb874a22a9aa0b0ef2dbcb7bd23ec7462
khaled <hkaleds3s@gmail.com>
2025-03-02 15:23:32 +0000

    Update tuxwisdom.py
    fixed spelling and added a bit of policy saying that the user can edit the software as they like(^_^)

--- tuxwisdom.py
+++ tuxwisdom.py
@@ -2,9 +2,11 @@ import os
 import time
 
 def main():
+    print("this is free software! edit it and distribute as much as you like(^_^)")
     print("how many times do you want tux to give a inspirational qoute")
     wisesays = int(input("$~"))
     other(wisesays)
+    pass
 
 def other(wisesays):
     for i in range(wisesays):
@@ -13,12 +15,14 @@ def other(wisesays):
         time.sleep(4)
         amnt + 1
         print(amnt)
+        pass
 
-    print("is that enough widom or do you want more")
+    print("is that enough wisdom or do you want more")
     yesornah = input("")
     if "n" in yesornah:
         main()
     elif "y" in yesornah:
         print("exiting")
+        pass
 
 main()
